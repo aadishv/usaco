8b057d3746b2367cf9664610cec4b705f6c6b08f
StarLight842 <842starlite@gmail.com>
2025-01-05 22:00:45 -0800

    intoblocks is killing me

--- generator.py
+++ generator.py
@@ -1,15 +1,16 @@
 import random
-def generate_test_case():
-    THRESHOLD = 10**9
-    a = random.randint(0, THRESHOLD)
-    b = random.randint(a, THRESHOLD)
-    c = random.randint(b, THRESHOLD)
-    nums = [a,b,c,a+b,b+c,c+a,a+b+c]
-    random.shuffle(nums)
-    nums = map(str, nums)
-    return " ".join(nums), f"{a} {b} {c}"
-NUM_TEST_CASES = 10
-for i in range(NUM_TEST_CASES):
-    t, a = generate_test_case()
-    print("TEST     :", t)
-    print("ANSWER   :", a)
+random.seed(42)
+# Generate test case
+def generate_test():
+    n = 500_000
+    q = 0
+
+    # Create a sequence with some patterns
+    sequence = []
+    for i in range(2, n+2):
+        sequence.append(i % n//1.5)
+    random.shuffle(sequence)
+
+    print(f"{n}, {q}")
+    print(" ".join(map(str, sequence)))
+generate_test();
--- main.cpp
+++ main.cpp
@@ -1,33 +1,18 @@
 #include <bits/stdc++.h>
-#include <cmath>
-#define ITER(v) v.begin(), v.end()
 using namespace std;
 
-void setIO(string name = ""){
+void setIO(string name = "", bool maxio = false) {
     if (name.size() > 0){
         freopen((name+".in").c_str(), "r", stdin);
         freopen((name+".out").c_str(), "w", stdout);
     }
-}
-int main() {
-    long long q;
-    cin >> q;
-    map<long long, long long> store;
-    for (long long i = 0; i < q; i++) {
-        long long op, a;
-        cin >> op >> a;
-        if (op == 0) {
-            long long v;
-            cin >> v;
-            store[a] = v;
-        } else if (op == 1) {
-            if (store.count(a)) {
-                cout << store[a] << endl;
-            } else {
-                cout << 0 << endl;
-            }
-        }
+    if (maxio) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
     }
-    cout << endl;
+}
+
+int main() {
+
     return 0;
 }
