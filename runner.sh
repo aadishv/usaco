1455f53d9def5f53c7cc325dee8c0e721d3228d4
StarLight842 <842starlite@gmail.com>
2025-08-03 14:40:51 -0700

    feat: minimal change to runner

--- runner.sh
+++ runner.sh
@@ -1,4 +1,4 @@
 clear && clang++ main.cpp -DAADISHV -std=c++17 -Wall -Wextra -O2 -lm \
-    && uv run test.py --input input.txt ./a.out
+    && uv run tester.py --input input.txt ./a.out
 # with debug symbols:
 # clear && clang++ main.cpp -DAADISHV -std=c++17 -Wall -Wextra -g -O0 -lm && time ./a.out < input.txt
